c5f8590167f84c5dc0b9d401ea18c4432380594f
yuriik83 <ykodjas@gmail.com>
2025-09-24 20:10:35 +0300

    Remove all files ignored by .gitignore

--- target/classes/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS patient (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    diagnosis VARCHAR(100),
-    room INT,
-    age INT
-);
